699e8ef121106f1cde01cc17a054964e4462b0b8
Shai <shai1@post.bgu.ac.il>
2022-05-30 09:38:19 +0300

    generate valid name for container and helm package

--- containers.py
+++ containers.py
@@ -4,7 +4,6 @@ import docker
 class Containers:
     def __init__(self):
         self.client = docker.from_env()
-        self.img=""
 
     def get_all_containers_name(self):
         running_container = self.client.containers.list(all=True)
@@ -14,7 +13,7 @@ class Containers:
     def pull_image(self, image_name):
         self.img = self.client.images.pull(image_name)
 
-    def run_container(self, container_name, port, target_port):
-        if container_name==self.img and self.img!="":
-            self.client.containers.run(self.img, detach=True, ports={port: target_port}, name=container_name)
-        return "can't run image, may be image not exist."
+    def run_container(self, image_name, container_name, port, target_port):
+       
+        self.client.containers.run(image_name, detach=True, ports={port: target_port}, name=container_name)
+        # return "can't run image, may be image not exist."
